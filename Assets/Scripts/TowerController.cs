5f12fd90427bfc7b80f693836ba39ef166a8ab9c
PBM42 <pbm764927@gmail.com>
2018-04-21 13:02:33 +0200

    feat: add tower controller

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+
+	private Camera _camera;
+	// Use this for initialization
+	void Start () {
+		_camera = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+		}
+	}
+}
