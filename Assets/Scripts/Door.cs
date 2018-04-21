8504328593ca628f2265d5167bf0c6fe7e77570e
Noxalus <noxalus@gmail.com>
2018-04-21 20:05:37 +0200

    Add doors.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public float closeInterval;
+    public float timeOffset;
+    public float openYPosition = -1;
+    public float closeYPosition = 1;
+
+    private float _closeCounter;
+    private bool _closed;
+
+	void Start ()
+    {
+        _closeCounter = closeInterval + timeOffset;
+        UpdateYPosition(openYPosition);
+        _closed = false;
+    }
+
+    void Update ()
+    {
+        _closeCounter -= Time.deltaTime;
+
+        if (_closeCounter < 0)
+        {
+            _closeCounter = closeInterval;
+            SwitchDoor();
+        }
+	}
+
+    private void SwitchDoor()
+    {
+        if (_closed)
+            UpdateYPosition(openYPosition);
+        else
+            UpdateYPosition(closeYPosition);
+
+        _closed = !_closed;
+    }
+
+    private void UpdateYPosition(float position)
+    {
+        var newPosition = transform.localPosition;
+        newPosition.y = position;
+        transform.localPosition = newPosition;
+    }
+}
