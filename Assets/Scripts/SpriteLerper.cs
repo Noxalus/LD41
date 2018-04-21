09972c273d61a5b9dd4f9ede1b6d4636af04b7e2
Julien Clouté <julien.cloute@gmail.com>
2018-04-21 12:45:15 +0200

    Add beat manager base to display a beat line.

---
+++ Assets/Scripts/SpriteLerper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLerper : MonoBehaviour {
+
+    public Vector2 startPos;
+    public Vector2 endPos;
+    public float duration;
+
+    private SpriteRenderer _sprite;
+    private float _elapsedTime;
+
+	// Use this for initialization
+	void Start () {
+        _elapsedTime = 0f;
+		_sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _elapsedTime += Time.deltaTime;
+        var alpha = _elapsedTime / duration;
+
+        Debug.Log("_elapsedTime: " + _elapsedTime);
+        Debug.Log("alpha: " + alpha);
+
+        if (alpha > 1f) {
+            // Destroy(this);
+            DestroyObject(gameObject);
+            return;
+        }
+
+        var pos = Vector2.Lerp(startPos, endPos, alpha);
+		transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+	}
+}
