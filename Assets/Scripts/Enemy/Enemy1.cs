87f602923eb1abae3bc70aa140cb9af75b386955
Noxalus <noxalus@gmail.com>
2018-04-21 12:51:27 +0200

    Add a simple enemy that goes to the exit using the nav mesh.

---
+++ Assets/Scripts/Enemy/Enemy1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy1 : MonoBehaviour {
+
+    public Transform target;
+
+    private NavMeshAgent _agent;
+
+	void Start ()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+	void Update ()
+    {
+        _agent.SetDestination(target.position);
+    }
+}
