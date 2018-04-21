2a4e93c5e810847f93d1311d4612cfd5253d21ab
PBM42 <pbm764927@gmail.com>
2018-04-21 13:18:16 +0200

    feat: add tower controller

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerController : MonoBehaviour {
+public class TowerController : MonoBehaviour
+{
 
-	private Camera _camera;
-	// Use this for initialization
-	void Start () {
-		_camera = Camera.main;
-	}
+  private Camera _camera;
+  private int _layerMask;
+  private int _maxRayDistance;
+  // Use this for initialization
+  void Start()
+  {
+    _camera = Camera.main;
+    _layerMask = 1 << 8;
+    _maxRayDistance = 11;
+  }
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-		}
-	}
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+      if (Physics.Raycast(ray, _maxRayDistance, _layerMask))
+      {
+        Debug.Log("Did Hit");
+      }
+    }
+  }
 }
