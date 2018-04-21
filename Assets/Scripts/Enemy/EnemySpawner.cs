d3f9fdb382253026310b2565d48f76e17de5243c
Noxalus <noxalus@gmail.com>
2018-04-21 13:24:43 +0200

    Add an enemy spawner.

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public Transform spawnTransform;
+    public Transform exitTransform;
+    public List<GameObject> enemyPrefabs;
+    public float spawnInterval;
+
+    private float _spawnCounter;
+
+	void Start ()
+    {
+        _spawnCounter = spawnInterval;
+    }
+
+    void Update ()
+    {
+        _spawnCounter -= Time.time;
+
+        if (_spawnCounter < 0)
+        {
+            SpawnEnemy();
+            _spawnCounter = spawnInterval;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        var newEnemy = Instantiate(enemyPrefabs[0], spawnTransform.position, spawnTransform.rotation);
+        newEnemy.GetComponent<Enemy1>().target = exitTransform;
+    }
+}
