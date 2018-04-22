1b2c3d3402e28c7d88dd8321719472c09fcb2ab0
Julien Clouté <julien.cloute@gmail.com>
2018-04-22 10:10:22 +0200

    Increase difficulty over time.

---
+++ Assets/Scripts/DifficultyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour {
+
+    public float minDifficulty;
+    public float maxDifficulty;
+    public float timeSpan;
+
+    public float difficulty { get {
+        return Mathf.Lerp(minDifficulty, maxDifficulty, Mathf.Clamp01(_time / timeSpan));
+    } }
+
+    private static float _time;
+
+	// Use this for initialization
+	void Start () {
+        _time = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_time += Time.deltaTime;
+	}
+}
