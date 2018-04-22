ae3097312ac3600554db100c5f4b2e10109b75e0
Noxalus <noxalus@gmail.com>
2018-04-22 10:57:36 +0200

    Add SoundManager + some sound effects + board panel texture.

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioClip enemyDeathSound;
+    public AudioClip enemyExitSound;
+    public AudioClip towerPlacedSound;
+    public AudioClip moneyEarnedSound;
+    public AudioClip beatMissedSound;
+
+    public AudioSource audioSource;
+
+	public void PlaySound(string soundName)
+    {
+        switch (soundName)
+        {
+            case "enemyDeath":
+                audioSource.clip = enemyDeathSound;
+                break;
+            case "enemyExitSound":
+                audioSource.clip = enemyExitSound;
+                break;
+            case "towerPlacedSound":
+                audioSource.clip = towerPlacedSound;
+                break;
+            case "moneyEarnedSound":
+                audioSource.clip = moneyEarnedSound;
+                break;
+            case "beatMissedSound":
+                audioSource.clip = beatMissedSound;
+                break;
+            default:
+                audioSource.clip = null;
+                break;
+        }
+
+        if (audioSource.clip)
+            audioSource.Play();
+    }
+}
