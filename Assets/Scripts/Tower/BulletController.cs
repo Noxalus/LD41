07bf99efc05c1a00741216f7016cbacca6e69405
Noxalus <noxalus@gmail.com>
2018-04-22 09:58:35 +0200

    Make sure to destroy tower's bullets when they hit an enemy.

--- Assets/Scripts/Tower/BulletController.cs
+++ Assets/Scripts/Tower/BulletController.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-
-  // Use this for initialization
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-  }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
 }
